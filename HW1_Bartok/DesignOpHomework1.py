15f3d0ef03c9f489325b2bf58237c1eecd73059d
kbartok <kbartok1@asu.edu>
2022-08-31 16:27:17 -0700

    Problem 1 Code complete, currently using 0,0,0,0,0 as intial guess

---
+++ HW1_Bartok/DesignOpHomework1.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.optimize import minimize
+# MAE598 Design Optimization HW1
+# Kevin Bartok kbartok1@asu.edu
+
+fun = lambda x: (x[0]-x[1])**2 + (x[1]+x[2]-2)**2 + (x[3]-1)**2 + (x[4]-1)**2
+
+cons = ({'type': 'eq', 'fun': lambda x: x[0] + 3*x[1]},
+        {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2*x[4]},
+        {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
+bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
+x0 = np.array([0, 0, 0, 0, 0])
+res = minimize(fun, x0, method='SLSQP', bounds=bnds, constraints=cons)
+
+print(res.fun)
