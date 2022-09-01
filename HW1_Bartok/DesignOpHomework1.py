ba0af5ab7adb06de4f1e2a6640f032532f41c89c
kbartok <kbartok1@asu.edu>
2022-08-31 21:30:05 -0700

    Problem 1 Code Complete, now putting in notebook

--- HW1_Bartok/DesignOpHomework1.py
+++ HW1_Bartok/DesignOpHomework1.py
@@ -1,7 +1,8 @@
-import numpy as np
-from scipy.optimize import minimize
 # MAE598 Design Optimization HW1
 # Kevin Bartok kbartok1@asu.edu
+import numpy as np
+from scipy.optimize import minimize
+
 
 fun = lambda x: (x[0]-x[1])**2 + (x[1]+x[2]-2)**2 + (x[3]-1)**2 + (x[4]-1)**2
 
@@ -9,7 +10,7 @@ cons = ({'type': 'eq', 'fun': lambda x: x[0] + 3*x[1]},
         {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2*x[4]},
         {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
 bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
-x0 = np.array([0, 0, 0, 0, 0])
+x0 = np.array([1, 2, 3, 4, 5])
 res = minimize(fun, x0, method='SLSQP', bounds=bnds, constraints=cons)
 
 print(res.fun)
