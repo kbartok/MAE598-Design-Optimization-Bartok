e833b323ba430baa6bca3c4d813f2a623a5204dc
kbartok <kbartok1@asu.edu>
2022-09-21 23:41:44 -0700

    Idk what's going on

--- HW2_Bartok/DesignOpHW2.py
+++ HW2_Bartok/DesignOpHW2.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+fun = lambda x: 10*x[1]**2 + 5*x[0]**2 - 8*x[0] - 14*x[1] + 12*x[0]*x[1] + 5
+def g(x):
+    return np.array([10*x[0]-8+12*x[1], 20*x[1]-14+12*x[0]])
+# Termination criteria
+eps = 1e-6
+sol = np.array([1, 1])
+alpha = 0.01
+i = 0
+error = [np.linalg.norm(g(sol))]
+while error[i] > eps:
+    sol = sol - alpha*g(sol)
+    print(sol)
+    i = i+1
+    error.append(np.linalg.norm(g(sol)))
+i = list(range(0, len(error)))
+plt.plot(i, np.log10(np.absolute(error)))
+plt.show()
+
+# ||gradient(x)|| < eplison (some small number) 1e-3 or 1e-6
+
+# calc f
+print("Newton")
+# Newton's method
+H = np.array([[10, 12], [12, 20]])
+sol = np.array([0, 0])
+e2 = np.linalg.norm(g(sol))
+while e2 > eps:
+    sol = sol - np.matmul(np.linalg.inv(H), g(sol))
+    e2 = np.linalg.norm(g(sol))
+    print(sol)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# calc phi
+
+# compare
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
